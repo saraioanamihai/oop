ca4f6ae6e7ebc99a235c0a64f9ec1feb8d640c59
Sara Ioana Mihai <mihaisara20@stud.ase.ro>
2022-12-03 00:07:28 +0200

    default constructor + constructor with parameters for FootballTicket

--- OOPProject/FootballTicket.cpp
+++ OOPProject/FootballTicket.cpp
@@ -2,10 +2,26 @@
 using namespace std;
 
 class FootballTicket {
-public:
+private:
 
 	string game; // ex. "Germany vs. Poland
 	char row;
 	int seat;
+
+public:
+
+	FootballTicket() {
+		game = "";
+		row = NULL;
+		seat = 0;
+	}
+
+	FootballTicket(string game, char row, int seat) {
+		this->game = game;
+		this->row = row;
+		this->seat = seat;
+	}
+
+
 
 };
