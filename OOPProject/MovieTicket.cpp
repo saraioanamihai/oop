009e026ce5dda6be0804338b6eb527373d14b805
Sara Ioana Mihai <mihaisara20@stud.ase.ro>
2022-12-02 23:44:15 +0200

    default constructor + constructor with parameters for MovieTicket

--- OOPProject/MovieTicket.cpp
+++ OOPProject/MovieTicket.cpp
@@ -7,4 +7,18 @@ private:
 	char row;
 	int seat;
 
+public:
+
+	MovieTicket() {
+		movieName = "";
+		row = NULL;
+		seat = 0;
+	}
+
+	MovieTicket(string movie, char row, int seat) {
+		this->movieName = movie;
+		this->row = row;
+		this->seat = seat;
+	}
+
 };
