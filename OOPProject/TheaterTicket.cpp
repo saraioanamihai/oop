b362416390711f74987c733b1fd89eaadcdee920
Sara Ioana Mihai <mihaisara20@stud.ase.ro>
2022-12-03 01:02:07 +0200

    adding and handeling the category for TheaterTicket

--- OOPProject/TheaterTicket.cpp
+++ OOPProject/TheaterTicket.cpp
@@ -6,6 +6,7 @@ class TheaterTicket {
 private: 
 
 	string playName;
+	enum categories { Category1, Category2, Box } category;
 	char row;
 	int seat;
 
@@ -13,18 +14,21 @@ public:
 
 	TheaterTicket() {
 		playName = "";
+		category = Category1;
 		row = NULL;
 		seat = 0;
 	}
 
-	TheaterTicket(string play, char row, int seat) {
+	TheaterTicket(string play, categories category, char row, int seat) {
 		this->playName = play;
+		this->category = category;
 		this->row = row;
 		this->seat = seat;
 	}
 
 	TheaterTicket(const TheaterTicket& t) {
 		this->playName = t.playName;
+		this->category = t.category;
 		this->row = t.row;
 		this->seat = seat;
 	}
@@ -37,6 +41,10 @@ public:
 		return playName;
 	}
 
+	categories getCategory() {
+		return category;
+	}
+
 	char getRow() {
 		return row;
 	}
@@ -47,6 +55,10 @@ public:
 
 	void setPlay(string play) {
 		this->playName = play;
+	}
+
+	void setCategory(categories category) {
+		this->category = category;
 	}
 
 	void setRow(char row) {
