1c8d0bfe527ee7147917d6ca6a930277e5ae80e9
Sara Ioana Mihai <mihaisara20@stud.ase.ro>
2022-12-03 02:47:47 +0200

    BIG CHANGE merging all 3 ticket classes into only one - Ticket

---
+++ OOPProject/Ticket.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+
+class Ticket {
+private:
+
+	const int id;
+	string event;
+	char row;
+	int seat;
+
+public:
+
+	Ticket() : id(rand()) {
+		event = "";
+		row = NULL;
+		seat = 0;
+	}
+
+	Ticket(string event, char row, int seat) : id(rand()) {
+		this->event = event;
+		this->row = row;
+		this->seat = seat;
+	}
+
+	Ticket(const Ticket& t) : id(t.id) {
+		this->event = t.event;
+		this->row = t.row;
+		this->seat = t.seat;
+	}
+
+	~Ticket() {
+	}
+
+	string getEvent() {
+		return event;
+	}
+
+	char getRow() {
+		return row;
+	}
+
+	int getSeat() {
+		return seat;
+	}
+
+	void setEvent(string movie) {
+		this->event = event;
+	}
+
+	void setRow(char row) {
+		this->row = row;
+	}
+
+	void setSeat(int seat) {
+		this->seat = seat;
+	}
+};
