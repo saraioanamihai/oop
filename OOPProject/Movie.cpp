e576233e91cd57ee27b2b91b96d218fa46389f1b
Sara Ioana Mihai <mihaisara20@stud.ase.ro>
2022-12-03 02:28:50 +0200

    copy constr, getters, setters for Movie

--- OOPProject/Movie.cpp
+++ OOPProject/Movie.cpp
@@ -6,53 +6,118 @@ private:
 
 	string movieTitle;
 	int noRows;
-	char* rows;
+	char* row;
 	int noSeats;
-	int* seats;
+	int* seat;
 
 public:
 
 	Movie() {
 		movieTitle = "";
 		noRows = 0;
-		rows = nullptr;
+		row = nullptr;
 		noSeats = 0;
-		seats = nullptr;
+		seat = nullptr;
 	}
 
-	Movie(string movieTitle, int noRows, char* rows, int noSeats, int* seats) {
+	Movie(string movieTitle, int noRows, char* row, int noSeats, int* seat) {
 		this->movieTitle = movieTitle;
-		if (noRows > 0 && rows != nullptr) {
-			this->rows = new char [noRows];
+		if (noRows > 0 && row != nullptr) {
+			this->row = new char [noRows];
 			for (int i = 0; i < noRows; i++) {
-				this->rows[i] = rows[i];
+				this->row[i] = row[i];
 			}
 			this->noRows = noRows;
 		}
 		else {
-			this->rows = nullptr;
+			this->row = nullptr;
 			this->noRows = 0;
 		}
-		if (noSeats > 0 && seats != nullptr) {
-			this->seats = new int[noSeats];
+		if (noSeats > 0 && seat != nullptr) {
+			this->seat = new int[noSeats];
 			for (int i = 0; i < noSeats; i++) {
-				this->seats[i] = seats[i];
+				this->seat[i] = seat[i];
 			}
 			this->noSeats = noSeats;
 		}
 		else {
-			this->seats = nullptr;
+			this->seat = nullptr;
 			this->noSeats = 0;
 		}
 	}
 
 	~Movie() {
-		if (rows != nullptr) {
-			delete[] rows;
+		if (row != nullptr) {
+			delete[] row;
 		}
-		if (seats != nullptr) {
-			delete[] seats;
+		if (seat != nullptr) {
+			delete[] seat;
 		}
 	}
 
+	Movie(const Movie& m) {
+		this->movieTitle = m.movieTitle;
+		if (m.noRows > 0 && m.row != nullptr) {
+			this->row = new char[m.noRows];
+			for (int i = 0; i < m.noRows; i++) {
+				this->row[i] = m.row[i];
+			}
+			this->noRows = m.noRows;
+		}
+		else {
+			this->row = nullptr;
+			this->noRows = 0;
+		}
+		if (m.noSeats > 0 && m.seat != nullptr) {
+			this->seat = new int[m.noSeats];
+			for (int i = 0; i < m.noSeats; i++) {
+				this->seat[i] = m.seat[i];
+			}
+			this->noSeats = m.noSeats;
+		}
+		else {
+			this->seat = nullptr;
+			this->noSeats = 0;
+		}
+	}
+
+	string getMovieTitle() {
+		return movieTitle;
+	}
+	
+	int getNoRows() {
+		return noRows;
+	}
+
+	char* getRow() {
+		return row;
+	}
+
+	int getNoSeats() {
+		return noSeats;
+	}
+
+	int* getSeat() {
+		return seat;
+	}
+
+	void setMovieTitle(string movieTitle) {
+		this->movieTitle = movieTitle;
+	}
+
+	void setNoRows(int noRows) {
+		this->noRows = noRows;
+	}
+
+	void setRow(char* row) {
+		this->row = row;
+	}
+
+	void setNoSeats(int noSeats) {
+		this->noSeats = noSeats;
+	}
+
+	void setSeat(int* seat) {
+		this->seat = seat;
+	}
 };
