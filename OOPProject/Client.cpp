8374d2b7e7efe61a4342c1e1b4aada7400ef83c9
Sara Ioana Mihai <mihaisara20@stud.ase.ro>
2022-12-04 20:10:17 +0200

    << and >> for Client

--- OOPProject/Client.cpp
+++ OOPProject/Client.cpp
@@ -35,15 +35,37 @@ public:
 		return surname;
 	}
 
-	void getName(string name) {
+	void setName(string name) {
 		if (name.size() > 0) {
 			this->name = name;
 		}
 	}
 
-	void getSurname(string surname) {
+	void setSurname(string surname) {
 		if (surname.size() > 0) {
 			this->surname = surname;
 		}
 	}
+
+	friend ostream& operator<<(ostream& out, Client c);
 };
+
+// we overload the << operator to print details about a client
+ostream& operator<<(ostream& out, Client c) {
+	out << "Name: " << c.name << endl;
+	out << "Surname: " << c.surname << endl;
+	return out;
+}
+
+// we overload the >> operator to read details about a client
+istream& operator>>(istream& in, Client& c) {
+	cout << "Name: ";
+	char buffer[100];
+	in >> buffer;
+	c.setName(buffer);
+	cout << "Surname: ";
+	char buffer1[100];
+	in >> buffer1;
+	c.setSurname(buffer1);
+	return in;
+}
