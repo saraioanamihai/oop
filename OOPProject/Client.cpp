74b4f3afbb71b76af0f38a70c348d49a2fa3bf66
Sara Ioana Mihai <mihaisara20@stud.ase.ro>
2022-12-04 20:05:44 +0200

    constructors and access methods for Client

--- OOPProject/Client.cpp
+++ OOPProject/Client.cpp
@@ -5,4 +5,45 @@ class Client {
 private: 
 	string name;
 	string surname;
+
+public:
+
+	Client() {
+		this->name = "";
+		this->surname = "";
+	}
+
+	Client(string name, string surname) {
+		this->name = name;
+		this->surname = surname;
+	}
+
+	~Client() {
+
+	}
+
+	Client(const Client& c) {
+		this->name = c.name;
+		this->surname = c.surname;
+	}
+
+	string getName() {
+		return name;
+	}
+
+	string getSurname() {
+		return surname;
+	}
+
+	void getName(string name) {
+		if (name.size() > 0) {
+			this->name = name;
+		}
+	}
+
+	void getSurname(string surname) {
+		if (surname.size() > 0) {
+			this->surname = surname;
+		}
+	}
 };
